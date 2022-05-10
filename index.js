9152da7842d03cb335bc78fc557e642471ee7ae3
Chase Parker <73307907+alanparkerc@users.noreply.github.com>
2022-05-09 21:50:27 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const axios = require("axios");
-
-const options = {
-  method: 'GET',
-  url: 'https://foo-fighters-news.p.rapidapi.com/news/antimusic',
-  headers: {
-    'X-RapidAPI-Host': 'foo-fighters-news.p.rapidapi.com',
-    'X-RapidAPI-Key': '4b95e0a240msh26e181aa8751bc0p18a5b2jsnf8a511ffe1e8'
-  }
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://foo-fighters-news.p.rapidapi.com/news",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Host": "foo-fighters-news.p.rapidapi.com",
+		"X-RapidAPI-Key": "4b95e0a240msh26e181aa8751bc0p18a5b2jsnf8a511ffe1e8"
+	}
 };
 
-axios.request(options).then(function (response) {
-	response.data.print();
-}).catch(function (error) {
-	console.error(error);
+$.ajax(settings).done(function (response) {
+	console.log(response);
 });
