98e9ef449ea35594a6dceb8ecce71659452e31ee
Chase Parker <73307907+alanparkerc@users.noreply.github.com>
2022-05-09 21:15:55 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const axios = require("axios");
+
+const options = {
+  method: 'GET',
+  url: 'https://foo-fighters-news.p.rapidapi.com/news/antimusic',
+  headers: {
+    'X-RapidAPI-Host': 'foo-fighters-news.p.rapidapi.com',
+    'X-RapidAPI-Key': '4b95e0a240msh26e181aa8751bc0p18a5b2jsnf8a511ffe1e8'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
